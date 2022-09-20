51b5f3983921a8c6b3ba6d5028cf52973fde9d57
saikiran <74893035+saikiran1201@users.noreply.github.com>
2022-09-20 20:05:55 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 from setuptools import find_packages
 from setuptools import setup
 
-setup(name='Tabular Data Cleaner',
+setup(name='TabularDataCleaner',
       version='1.0',
       description='Data Cleaner',
       author='Sai Kiran Pasuvula',
