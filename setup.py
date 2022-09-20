a81266b942f18883006325652c89a36e4ecd0465
saikiran <74893035+saikiran1201@users.noreply.github.com>
2022-09-20 19:53:00 +0530

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+from setuptools import find_packages
+from setuptools import setup
+
+setup(name='Tabular Data Cleaner',
+      version='1.0',
+      description='Data Cleaner',
+      author='Sai Kiran Pasuvula',
+      author_email='pasulakiransai@gmail.com',
+     )
