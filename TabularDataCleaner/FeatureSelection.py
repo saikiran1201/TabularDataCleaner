3b4c5cd51c020b61f82079d7188029853fefacc1
saikiran <74893035+saikiran1201@users.noreply.github.com>
2024-07-15 22:35:40 +0530

    Update FeatureSelection.py

--- TabularDataCleaner/FeatureSelection.py
+++ TabularDataCleaner/FeatureSelection.py
@@ -3,13 +3,22 @@ import pandas as pd
 from sklearn.feature_selection import mutual_info_classif, chi2, f_classif
 from sklearn.preprocessing import LabelEncoder
 from feature_engine.selection import SelectByInformationValue
+from sklearn.inspection import permutation_importance
+from catboost import CatBoostClassifier, CatBoostRegressor, Pool
+# import eli5
+import shap
 
 class FeatureSelector:
+    # For Classification problem
     def __init__(self):
         self.mi = None
         self.fisher = None
         self.chi = None
         self.iv = None
+        self.FI = None
+        self.shap = None
+        self.PI = None
+        # self.eli5 = None
 
     def mutual_information(self, X, y):
         mi = mutual_info_classif(X, y)
@@ -36,9 +45,72 @@ class FeatureSelector:
         iv_series = iv_series.sort_values(ascending=False)
         return iv_series
 
+    def FI_with_catboost(self, X, y):
+        categorical_features = X.select_dtypes(exclude=[np.number]).columns.tolist()
+        train_pool = Pool(X, y, cat_features = categorical_features)
+        if isinstance(y, np.ndarray) and np.issubdtype(y.dtype, np.number):
+            estimator = CatBoostRegressor(iterations=500, max_depth=5, learning_rate=0.05, random_seed=1066, logging_level='Silent')
+        else:
+            estimator = CatBoostClassifier(iterations=500, max_depth=5, learning_rate=0.05, random_seed=1066, logging_level='Silent')
+        model = estimator.fit(train_pool)
+        if isinstance(y, np.ndarray) and np.issubdtype(y.dtype, np.number):
+            estimator = CatBoostRegressor(iterations=500, max_depth=5, learning_rate=0.05, random_seed=1066, logging_level='Silent')
+        else:
+            estimator = CatBoostClassifier(iterations=500, max_depth=5, learning_rate=0.05, random_seed=1066, logging_level='Silent')
+        model = estimator.fit(train_pool)   
+        FI_series = pd.Series(model.get_feature_importance(train_pool,), X.columns)
+        return FI_series
+    
+    # def FI_with_eli5(self, X, y):
+    #     categorical_features = X.select_dtypes(exclude=[np.number]).columns.tolist()
+    #     train_pool = Pool(X, y, cat_features = categorical_features)
+    #     if isinstance(y, np.ndarray) and np.issubdtype(y.dtype, np.number):
+    #         estimator = CatBoostRegressor(iterations=500, max_depth=5, learning_rate=0.05, random_seed=1066, logging_level='Silent')
+    #     else:
+    #         estimator = CatBoostClassifier(iterations=500, max_depth=5, learning_rate=0.05, random_seed=1066, logging_level='Silent')
+    #     model = estimator.fit(train_pool)
+    #     Series = eli5.explain_weights_catboost(catb = model,
+    #                  pool = train_pool,
+    #                  )
+    #     Series_ = eli5.formatters.as_dataframe.format_as_dataframe(Series)
+    #     eli5_series = pd.Series(data=Series_['weight'].values, index=Series_['feature'])
+    #     eli5_series.name = None
+    #     eli5_series.index.name = None
+    #     return eli5_series
+
+    def FI_with_shap(self, X, y):
+        categorical_features = X.select_dtypes(exclude=[np.number]).columns.tolist()
+        train_pool = Pool(X, y, cat_features = categorical_features)
+        if isinstance(y, np.ndarray) and np.issubdtype(y.dtype, np.number):
+            estimator = CatBoostRegressor(iterations=500, max_depth=5, learning_rate=0.05, random_seed=1066, logging_level='Silent')
+        else:
+            estimator = CatBoostClassifier(iterations=500, max_depth=5, learning_rate=0.05, random_seed=1066, logging_level='Silent')
+        model = estimator.fit(train_pool)
+        # Accepts only tree based models
+        explainer = shap.TreeExplainer(model)
+        shap_values = explainer.shap_values(X)
+        vals = np.abs(shap_values).mean(0)
+        shap_series = pd.Series(vals, X.columns)
+        # shap.summary_plot(shap_values, X, plot_type='bar')
+        return shap_series
+    
+
+
+    def P_imp(self, X, y):
+        categorical_features = X.select_dtypes(exclude=[np.number]).columns.tolist()
+        train_pool = Pool(X, y, cat_features = categorical_features)
+        if isinstance(y, np.ndarray) and np.issubdtype(y.dtype, np.number):
+            estimator = CatBoostRegressor(iterations=500, max_depth=5, learning_rate=0.05, random_seed=1066, logging_level='Silent')
+        else:
+            estimator = CatBoostClassifier(iterations=500, max_depth=5, learning_rate=0.05, random_seed=1066, logging_level='Silent')
+        model = estimator.fit(train_pool)
+        perm_importance = permutation_importance(model, X, y, n_repeats=10, random_state=1066)
+        PI_series = pd.Series(perm_importance.importances_mean, X.columns)
+        return PI_series
+
+
     def fit(self, X, y):
         print("Calculating Fisher Score for numerical features...")
-        
         # Separate numerical and categorical features
         numerical_features = X.select_dtypes(include=[np.number])
         categorical_features = X.select_dtypes(exclude=[np.number])
@@ -58,8 +130,10 @@ class FeatureSelector:
             self.mi = pd.Series([], name="Mutual Information")
             self.chi = pd.Series([], name="Chi-Square")
 
-        # Calculate Information Value
         self.iv = self.information_value(X, y)
+        self.FI = self.FI_with_catboost(X, y)
+        self.PI = self.P_imp(X, y)
+        self.shap = self.FI_with_shap(X, y) 
 
         # Creating a DataFrame to compile all results
         results = pd.DataFrame(index=X.columns)
@@ -67,16 +141,23 @@ class FeatureSelector:
         results['Mutual Information'] = self.mi
         results['Chi-Square'] = self.chi
         results['Information Value'] = self.iv
+        results['FI Value'] = self.FI
+        results['Pimp Value'] = self.PI
+        results['Shap Value'] = self.shap
 
         # Ranking features based on each method
         results['Fisher Rank'] = results['Fisher Score'].rank(ascending=False, method='min')
         results['MI Rank'] = results['Mutual Information'].rank(ascending=False, method='min')
         results['Chi Rank'] = results['Chi-Square'].rank(ascending=False, method='min')
         results['IV Rank'] = results['Information Value'].rank(ascending=False, method='min')
+        results['FI Rank'] = results['FI Value'].rank(ascending=False, method='min')
+        results['Pimp Rank'] = results['Pimp Value'].rank(ascending=False, method='min')
+        results['Shap Rank'] = results['Shap Value'].rank(ascending=False, method='min')
 
         # Aggregating the ranks to get a combined rank
-        results['Average Rank'] = results[['Fisher Rank', 'MI Rank', 'Chi Rank', 'IV Rank']].mean(axis=1)
+        results['Average Rank'] = results[['Fisher Rank', 'MI Rank', 'Chi Rank', 'IV Rank', "FI Rank", "Pimp Rank",
+                                           'Shap Rank', 
+                                           # 'Eli5 Rank'
+                                          ]].mean(axis=1)
         results = results.sort_values('Average Rank')
-
         return results
-
